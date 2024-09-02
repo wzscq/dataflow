56f787a86657a7a335af82d95f4be1b9b4a4e492
wangzhongshu <wzscq@126.com>
2024-09-02 11:33:03 +0800

    commit changes

--- build/build_dockerimage.sh
+++ build/build_dockerimage.sh
@@ -1,3 +1,3 @@
 docker image rm wangzhsh/dataflow:0.1.0
-docker build . -t wangzhsh/dataflow:0.1.0
-docker push wangzhsh/dataflow:0.1.0
+docker build . -t registry.cn-hangzhou.aliyuncs.com/wangzhsh/dataflow:0.1.0
+docker push registry.cn-hangzhou.aliyuncs.com/wangzhsh/dataflow:0.1.0
